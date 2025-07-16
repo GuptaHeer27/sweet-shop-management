48b52a661cc6dc052039170ed8dccc3abc5fb2dc
GuptaHeer27 <guptaheer27@gmail.com>
2025-07-16 18:33:27 +0530

    Implement Search sweet by Name

--- src/main/java/com/incubyte/sweetshop/SweetShopService.java
+++ src/main/java/com/incubyte/sweetshop/SweetShopService.java
@@ -7,22 +7,36 @@ public class SweetShopService {
 
     private Map<String, Sweet> inventory = new HashMap<>();
 
+    
+    // Add Sweets
     public void addSweet(Sweet sweet) {
         inventory.put(sweet.getId(), sweet);
     }
 
+    // View Sweets
     public List<Sweet> getAllSweets() {
         return new ArrayList<>(inventory.values());
     }
 
+    // Delete Sweets
     public boolean deleteSweet(String id) {
         return inventory.remove(id) != null;
     }
     
-    // Just to run method in Test
-    public List<Sweet> searchByName(String category) { 
-        throw new UnsupportedOperationException("Not implemented yet");
+    
+    // Search sweets by Name
+    public List<Sweet> searchByName(String keyword) {
+        List<Sweet> result = new ArrayList<>();
+
+        for (Sweet sweet : inventory.values()) {
+            if (sweet.getName().toLowerCase().contains(keyword.toLowerCase())) {
+                result.add(sweet);
+            }
+        }
+
+        return result;
     }
+
 
     
 
